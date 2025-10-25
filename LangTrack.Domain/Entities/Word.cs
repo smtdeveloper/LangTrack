469f46daabaa0dcf68bba62b5d9a27be33ecd91e
Samet Akca <smtakca99@gmail.com>
2025-10-25 23:21:36 +0300

    Word

---
+++ LangTrack.Domain/Entities/Word.cs
@@ -0,0 +1,10 @@
+﻿namespace LangTrack.Domain.Entities;
+
+public class Word : BaseEntity
+{
+    public string Text { get; set; } = default!;
+    public string Meaning { get; set; } = default!;
+    public string? Example { get; set; }
+    public string? Tags { get; set; } // "phrasal,verb,b2" gibi
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
